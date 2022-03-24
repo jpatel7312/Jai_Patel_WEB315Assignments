8c213003b0099374c6375618ff7e6201b17794fb
jpatel7312 <jaypatel7312@gmail.com>
2022-03-24 14:14:52 -0400

    adds requires files and grocery store component and adds component to left navigation bar as well as on the landing page but hides few elements on the landing page

---
+++ JayPatelGroceryApp/JayPatelGroceryApp/FoodItem.cs
@@ -0,0 +1,10 @@
+﻿namespace JayPatelGroceryApp
+{
+    public class FoodItem
+    {
+            public int foodQuantity {get; set;}
+            public string foodName { get; set; }
+            public string foodDescription { get; set; }
+            public string foodType { get; set; }
+    }
+}
---
+++ JayPatelGroceryApp/JayPatelGroceryApp/GroceryIsle.cs
@@ -0,0 +1,9 @@
+﻿namespace JayPatelGroceryApp
+{
+    public class GroceryIsle
+    {
+        public string isleName { get; set; }
+        public int isleNum { get; set; }
+        public string foodList { get; set; }
+    }
+}
