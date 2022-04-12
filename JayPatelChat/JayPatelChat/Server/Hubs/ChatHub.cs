e2843b2b9c4f25d6047768a393f810d24e15f14f
jpatel7312 <jaypatel7312@gmail.com>
2022-04-12 19:04:18 -0400

    adds working chat app need to work on istyping

---
+++ JayPatelChat/JayPatelChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JayPatelChat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task SendWhoIsTyping(string user)
+        {
+            await Clients.All.SendAsync("RecieveWhoIsTyping", user);
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
